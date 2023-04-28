b71800eca312e388c0f05dd62bc9b9420a59c2bb
Tengiz Sharafiev <btolfa@gmail.com>
2023-04-28 09:38:17 +0300

    arrays

---
+++ Rust_Tutorial/src/arrays.rs
@@ -0,0 +1,16 @@
+pub fn run() {
+    let mut numbers: [i32; 4] = [1, 2, 3, 4];
+
+    numbers[2] = 20;
+
+    println!("{:?}", numbers);
+
+    println!("Single Value: {}", numbers[0]);
+
+    println!("Array Length: {}", numbers.len());
+
+    println!("Array occupies {} bytes", std::mem::size_of_val(&numbers));
+
+    let slice: &[i32] = &numbers[1..3];
+    println!("Slice: {:?}", slice);
+}
